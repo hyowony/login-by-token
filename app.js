67c6c6b71f727fb47f07a24a85cdb72be5130149
hyowony <118158856+hyowony@users.noreply.github.com>
2022-12-30 21:57:28 +0900

    수업 마치고 이후에 필기노트 1차

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser')
 const app = express();
+
 const port = 3000;
+
+app.use(cookieParser())
+app.use(express.json())
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 열렸어요!');
 });
 
+const sessions push
+
+
+// const userid = req.cookies.userid
 app.get('/users', (req, res) => {
-  res.send('유저')
+  const userid = req.cookies.userid
+  const user = users.find(user => user.id ===userId)
+  console.log(userid)
+  res.send()
 })
 
 app.post('/login', (req, res) => {
-  res.send('로그인')
+  const userid 
 })
 
 app.post('/logout', (req, res) => {
