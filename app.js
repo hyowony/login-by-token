401216a16a1c16c54bf0b4b2094e1c9c79bb2d54
hyowony <118158856+hyowony@users.noreply.github.com>
2023-01-01 01:07:20 +0900

    세션 로직 직접 만든 파일은 app.js고 익스프레스 세션은 test.js입니다

--- app.js
+++ app.js
@@ -3,42 +3,53 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const port = 3000;
 
+///쿠키 파서를 쓴다는 구문이다.
 app.use(cookieParser())
+///const로 설정된 app이 express를 사용해서. json을 쓸 것이다.
 app.use(express.json())
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 열렸어요!');
 });
 
+/// 우리가 할당한 user 데이터이다. 
 const user = [ 
   {id:"noggong"}
   {id:"hyowon"}
   {id:"kimin"}
 ]
 
+//세션에 값을 담을 거라는 의미이다.
 const sessions = []
 // const userid = req.cookies.userid
+
 app.get('/users', (req, res) => {
+  ///userid는 요청한다 쿠키를 가지고 userid로 접근한다.
   const userid = req.cookies.userid
+  ///user는 찾는 것이다. 세션을 가지고 찾는 조건은 세션에서 ssid가 쿠키를 가지고 요청했을 때 ssid와 같은 것을 찾는 것이다.
   const user = seesions.find(session => session.ssid === req.cookies.ssid)
+  ///세션과 유저를 한번 찍어봐라 
   console.log(sessions)
   console.log(user)
+  ///서버야 응답해봐 id는 user에 접속 했을 때 id로 호출되는 것이다.
   res.send({id:user.id})
 })
 
 app.post('/login', (req, res) => {
+  ///userId는 요청하는 것이다 바디를 타고 들어가서 userId로 접속해라
   const userId = req.body.userId
+  ///유저스는 찾는거다. user인 것을 user에서 id로 접속했을 때 userId와 일치하는 것이다.
   const user = users.find(user => user.id ===userId)
-
+///ssid는 시간은 지금이고, string 형식으로 호출된다(?)
   const ssid = Date.now().toString()
-
+///세션에 넣어라 ... 위 데이터 그대로 ssid를 사용해서(?) - 이건 잘 모르겠습니다.
   session.push({
     ...user,
     ssid
   })
-
+///쿠키를 보내봐
   res.cookie("ssid",ssid)
-
+//우리가 user.id로 줄게
   res.send(user.id)
 })
 
@@ -49,3 +60,5 @@ app.post('/logout', (req, res) => {
 app.post('/register', (req, res) => {
   res.send('레지스터.')
 })
+
+
--- test.js
+++ test.js
@@ -1,39 +1,50 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
+///session은 express-session이라는 모듈이 필요하다. npm i express-session하면 설치할 수 있다.
 const session = require('express-session')
+/// 위와 마찬가지로 memorystore을 설치한다.
 const Memorystore = require('memorystore')(session);
 
 const app = express();
 const port = 3000;
-
+///express가 서버를 실행할 때 같이 실행되는 것들을 의미한다. 서버가 실행되면 쿠키 파서와 json 형식을 쓸 수 있다
 app.use(cookieParser())
 app.use(express.json())
 
+///세션 객체 데이터 모음 시크릿 키 발급 과정이다. 
 const sessionobj = {
   secret : 'kong',
   resave : false,
+  //초기화불가 설정 저장
   saveUnuninitialized: true,
+  ///세션 교환소?는 새롭게 메모리스토어에서 가져온다. 
   store: new Memorystore(),
 };
-
+/// 세션을 사용하는 과정이다. 
 app.use(session(sessionobj))
-
+/// 우리가 설정한 데이터
 const users = [
   {id: "noggong"},
   {id: "hyowon"},
   {id: "kimin"}
 ]
 
+/// 세션을 통해 전달된 데이터를 찾는 과정이다.
 app.get('/users', (req,res)=> {
+  ///유저 아이디는 세션을 통해 유저 아이디라는 것을 통해 실행된다.
   const userId = req.session.userId
+  ///유저는 유저에서 id인 값과 유저id가 일치하면 찾아온다.
   const user = users.find(user => user.id === userId )
+  ///응답해서 유저를 가져와라
   res.send(user)
 })
-
+///로그인은 로직상 포스트로 정보를 보내줘야 한다. 웹페이지의 바디에 정보를 보내는 과정이다. 
 app.post('/login', (req,res)=> {
+  //user아이디는 바디를 타고 들어가서 user.id인것을 찾는다. 
   const userId = req.body.userId
+  // 유저는 찾는 것이다 user.id가 userId와 일치하는 것을 찾는 것이지.
   const user = users.find(user => user.id ===userId)
-
+//세션 발급 과정 
   res.session.userid = user.id
   res.send(user.id)
 })
